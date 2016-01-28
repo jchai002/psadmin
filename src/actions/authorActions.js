16f371ffbf494ab272b99d4de8875192528f7bce
Jerry Chai <jchai002@gmail.com>
2016-01-28 10:44:04 -0800

    add delete action

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -13,6 +13,26 @@ var AuthorActions = {
     });
 
     console.log('hit create new author action')
+  },
+  updateAuthor: function(author){
+    var updatedAuthor = AuthorApi.saveAuthor(author);
+
+    Dispatcher.dispatch({
+      actionType: ActionTypes.UPDATE_AUTHOR,
+      author: updatedAuthor
+    });
+
+    console.log('hit update author action')
+  },
+  deleteAuthor: function(id){
+    AuthorApi.deleteAuthor(id);
+
+    Dispatcher.dispatch({
+      actionType: ActionTypes.DELETE_AUTHOR,
+      id: id
+    });
+
+    console.log('hit delete author action')
   }
 }
 
